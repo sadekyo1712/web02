80bd7e3cd8a4993f984d6d900b9b96c0ae312a43
sadekyo1712 <buiduchung.bkhn@gmail.com>
2018-01-22 15:27:19 +0700

    .

--- web02/config.py
+++ web02/config.py
@@ -2,13 +2,13 @@
 config = {
     'url': 'http://frontend.test.com',
     'url2': 'http://admin.test.com',
-    'vulnerable_url': 'http://admin.test.com/addnews.php?user_id=15',  # vul url contain blind sqli
+    'vulnerable_url': 'http://admin.test.com/addnews.php?user_id=15',  # vuln url contain blind sqli
     'script_name': 'shelln3#%n446nmn!#&*^',
     'interupt': 'n3#%n446nmn!#&*^',
     'git_clone_tunna': 'git clone https://github.com/sadekyo1712/Tunna',
     'run_tunna_server': 'python Tunna/webserver.py',
-    'upload_tunna': False,
-    'use_tunna_server': False,
+    'upload_tunna': True,
+    'use_tunna_server': True,
     'url_exploit': 'http://admin.test.com/addnews.php?user_id=15',
     'signal_str': 'Khanh Nguyen',  # signal to assert right respone from webserver
     'data': '',
